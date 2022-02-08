85ce3181ddf2a8b7177f63ae3d689e2c0c9fcbf8
jjj999 <jjj999to@gmail.com>
2022-02-09 00:28:42 +0900

    Implemented some commands

--- tpg26x/tpg26x.py
+++ tpg26x/tpg26x.py
@@ -1,4 +1,5 @@
 import enum
+from functools import cached_property
 import io
 import typing as t
 
@@ -146,6 +147,89 @@ def _search_measurement_status(value: int) -> t.Optional[MeasurementStatus]:
     return None
 
 
+
+class GaugeID(int, enum.Enum):
+
+    TPR = b"TPR"
+    """Pirani Gauge or Pirani Capacitive gauge"""
+
+    IK9 = b"IK9"
+    """Cold Cathode Gauge 10^-9"""
+
+    IKR11 = b"IKR11"
+    """Cold Cathode Gauge 10^-11"""
+
+    PKR = b"PKR"
+    """FullRange CC Gauge"""
+
+    PBR = b"PBR"
+    """FullRange BA Gauge"""
+
+    IMR = b"IMR"
+    """Pirani / High Pressure Gauge"""
+
+    CMR = b"CMR"
+    """Linear gauge"""
+
+    NO_SENSOR = b"noSEn"
+    """"No sensor"""
+
+    NO_IDENTIFIER = b"noid"
+    """No identifier"""
+
+
+def _search_gauge_id(value: bytes) -> t.Optional[GaugeID]:
+    for gauge_id in GaugeID:
+        if gauge_id == value:
+            return gauge_id
+    return None
+
+
+class GaugeType(bytes, enum.Enum):
+
+    GAUGE1 = b"0"
+    GAUGE2 = b"1"
+
+
+class ErrorStatus(bytes, enum.Enum):
+
+    NO_ERROR = b"0000"
+    ERROR = b"0000"
+    NO_HARDWARE = b"0100"
+    INADMISSIBLE_PARAMETER = b"0010"
+    SYNTAX_ERROR = b"0001"
+
+
+def _search_error_status(value: bytes) -> t.Optional[ErrorStatus]:
+    for status in ErrorStatus:
+        if status == value:
+            return status
+    return None
+
+
+class ResetErrorStatus(bytes, enum.Enum):
+
+    NO_ERROR = b"0"
+    WATCHDOG_RESPONDED = b"1"
+    TASK_FAILED = b"2"
+    EPROM_ERROR = b"3"
+    RAM_ERROR = b"4"
+    EEPROM_ERROR = b"5"
+    DISPLAY_ERROR = b"6"
+    AD_CONVERTER_ERROR = b"7"
+    GAUGE1_ERROR = b"9"
+    GAUGE1_IDENTIFICATION_ERROR = b"10"
+    GAUGE2_ERROR = b"11"
+    GAUGE2_IDENTIFICATION_ERROR = b"12"
+
+
+def _search_reset_error_status(value: bytes) -> ResetErrorStatus:
+    for status in ResetErrorStatus:
+        if status == value:
+            return status
+    return None
+
+
 class Tpg26x:
 
     END_OF_TEXT = b"\x03"
@@ -173,10 +257,10 @@ class Tpg26x:
     def _write(self, *args: bytes) -> int:
         return self._serial.write(self._format(*args))
 
-    def _enquiry(self) -> None:
+    def enquiry(self) -> None:
         self._serial.write(self.ENQUIRY)
 
-    def _readline(self) -> bytes:
+    def readline(self) -> bytes:
         data = self._serial.readline()
         if data[-2:] == self.NEWLINE:
             return data[:-2]
@@ -191,6 +275,11 @@ class Tpg26x:
             raise IOError(f"Mnemonic {mnemonic} was forbiddened by the TPG26x.")
         else:
             raise IOError(f"Unexpected data was received: {data}")
+
+    def send_command(self, mnemonic: Mnemonics, *args: bytes) -> None:
+        self._write(mnemonic.value, *args)
+        ack = self.readline()
+        self._handle_ack(ack, mnemonic)
 
     @staticmethod
     def _parse_pressure(raw: bytes) -> float:
@@ -217,12 +306,9 @@ class Tpg26x:
         return (status, pressure1, pressure2)
 
     def _read_gauge(self, mnemonic: Mnemonics) -> t.Tuple[MeasurementStatus, float]:
-        self._write(mnemonic)
-        ack = self._readline()
-        self._handle_ack(ack, mnemonic)
-        self._enquiry()
-
-        data = self._readline()
+        self.send_command(mnemonic)
+        self.enquiry()
+        data = self.readline()
         return self._parse_measurement(data)
 
     def read_gauge1(self) -> t.Tuple[MeasurementStatus, float]:
@@ -232,16 +318,10 @@ class Tpg26x:
         return self._read_gauge(Mnemonics.PR2)
 
     def read_both(self) -> None:
-        self._write(Mnemonics.PRX)
-        ack = self._readline()
-        self._handle_ack(ack, Mnemonics.PRX)
-        self._enquiry()
-
-        data = self._readline()
+        self.send_command(Mnemonics.PRX)
+        self.enquiry()
+        data = self.readline()
         return self._parse_measurements(data)
-
-    def log(self) -> None:
-        pass
 
     def _turn_on_off(
         self,
@@ -261,46 +341,85 @@ class Tpg26x:
         else:
             signal2 = b"2"
 
-        self._write(Mnemonics.SEN, signal1, signal2)
-        ack = self._readline()
-        self._handle_ack(ack, Mnemonics.SEN)
-
-        data = self._readline()
+        self.send_command(Mnemonics.SEN, signal1, signal2)
+        self.enquiry()
+        data = self.readline()
         status1, status2 = data.split(b",")
-        if (status1 == signal1) and (status2 == signal2):
-            return
 
-        raise IOError(
-            "Gauges cannot be turned on or off.\n"
-            f"Status: gauge 1 -> {status1}, gauge2 -> {status2}"
-        )
+        if (status1 != signal1) or (status2 != signal2):
+            raise IOError(
+                "Gauges cannot be turned on or off.\n"
+                f"Status: gauge 1 -> {status1}, gauge2 -> {status2}"
+            )
 
     def turn_on_gauge1(self) -> None:
-        self._turn_on(True, False)
+        self._turn_on_off(True, None)
 
     def turn_on_gauge2(self) -> None:
-        self._turn_on(False, True)
+        self._turn_on_off(None, True)
 
     def turn_on_both(self) -> None:
         self._turn_on(True, True)
 
-    def _turn_off(self, gauge1: bool, gauge2: bool) -> None:
-        if gauge1:
-            signal1 = b"1"
-        else:
-            signal1 = b"0"
-        if gauge2:
-            signal2 = b"1"
-        else:
-            signal2 = b"0"
-
-        self._write(Mnemonics.SEN, signal1, signal2)
-
     def turn_off_gauge1(self) -> None:
-        self._turn_off(True, False)
+        self._turn_off(False, None)
 
     def turn_off_gauge2(self) -> None:
-        self._turn_off(False, True)
+        self._turn_off(None, False)
 
     def turn_off_both(self) -> None:
-        self._turn_off(True, True)
+        self._turn_off(False, False)
+
+    def _get_gauge_ids(self) -> t.Tuple[GaugeID, GaugeID]:
+        self.send_command(Mnemonics.TID)
+        self.enquiry()
+        data = self.readline()
+        id_gauge1, id_gauge2 = map(_search_gauge_id, data.split(b","))
+
+        cls = type(self)
+        cls.id_gauge1.func = lambda _: id_gauge1
+        cls.id_gauge2.func = lambda _: id_gauge2
+
+        return (id_gauge1, id_gauge2)
+
+    @cached_property
+    def id_gauge1(self) -> None:
+        return self._get_gauge_ids()[0]
+
+    @cached_property
+    def id_gauge2(self) -> None:
+        return self._get_gauge_ids()[1]
+
+    def _change_channel(self, channel: GaugeType) -> GaugeType:
+        if channel not in {b"0", b"1"}:
+            raise ValueError(f"'channel' must be 0 or 1 as bytes.")
+
+        self.send_command(Mnemonics.SCT, channel)
+        self.enquiry()
+        data = self.readline()
+        return GaugeType.GAUGE1 if data == b"0" else GaugeType.GAUGE2
+
+    def change_channel_1(self) -> None:
+        channel = self._change_channel(GaugeType.GAUGE1)
+        if channel != GaugeType.GAUGE1:
+            raise IOError("Measurement channel wasn't changed appropriately.")
+
+    def change_channel_2(self) -> None:
+        chnnel = self._change_channel(GaugeType.GAUGE2)
+        if chnnel != GaugeType.GAUGE2:
+            raise IOError("Measurement channel wasn't changed appropriately.")
+
+    def get_error_status(self) -> ErrorStatus:
+        self.send_command(Mnemonics.ERR)
+        self.enquiry()
+        data = self.readline()
+        status = _search_error_status(data)
+        if status is not None:
+            return status
+        raise IOError(f"Unexpected binary was received: {data}")
+
+    def reset(self) -> t.List[ResetErrorStatus]:
+        self.send_command(Mnemonics.RES, b"1")
+        self.enquiry()
+        data = self.readline()
+        return [_search_reset_error_status(s) for s in data.split(b",")]
